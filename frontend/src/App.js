b7731e1edd458229c970c19217269e04045db177
monolithdragon <joo.lajos76@gmail.com>
2022-05-27 12:58:28 +0200

    Added promotions to the project.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,15 @@ import { ThemeProvider } from '@mui/system';
 import Appbar from './components/appbar';
 import theme from './styles/theme';
 import Banner from './components/banner';
+import Promotions from './components/promotions';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Container maxWidth='xl' sx={{ background: '#fff' }}>
         <Appbar /> 
-        <Banner />       
+        <Banner />
+        <Promotions />      
       </Container>
     </ThemeProvider>
   );
